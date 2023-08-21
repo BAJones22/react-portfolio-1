22a292ab5d3b7cd8cf8ba1808e42bc3bbbb7870c
Brooke Adlesperger-Jones <badles@live.com>
2023-08-21 17:18:07 -0500

    updated home.js section

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,31 @@
 import React from "react";
+import BannerBackground from "../Assets/home-banner-background.png";
+import BannerImage from "../Assets/home-banner-image.png";
 import Navbar from "./Navbar";
+import { FiArrowRight } from "react-icons/fi";
 
 const Home = () => {
-  return;
-  <div className="home-container">
-    <Navbar />
-  </div>;
+  return (
+    <div className="home-container">
+      <Navbar />
+      <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+          <img src={BannerBackground} alt="" />
+        </div>
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+            Hello! I'm Brooke.
+          </h1>
+          <p className="primary-text">
+            Junior Full-Stack Developer
+          </p>
+        </div>
+        <div className="home-image-section">
+          <img src={BannerImage} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
